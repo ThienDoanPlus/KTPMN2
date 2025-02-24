e40660dde9a14f4e14f4554cb83ea7ed61f5269e
NTD <nguyenthiendoan2Gmail.com>
2025-02-24 13:46:34 +0700

    hi

---
+++ BTHBuoi1/src/main/java/daihocmo/bthbuoi1/BTHBuoi1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daihocmo.bthbuoi1;
+
+/**
+ *
+ * @author admin
+ */
+public class BTHBuoi1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+    }
+}
